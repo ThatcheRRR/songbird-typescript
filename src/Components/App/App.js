171bbc9b656523774cd4e6eebc98eb4c05d9d833
ThatcheRRR <pavelosipov777@mail.ru>
2020-08-01 20:13:33 +0300

    feat: add data file

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Header from './../Header/Header';
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <div className = 'test'>
+                test
+                <Header />
+            </div>
+        )
+    }
+}
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+    return(
+        <header>
+            test header
+        </header>
+    )
+}
+
+export default Header;
