5a92e651fe9ab0462ef755e8fc54e155dc11d629
ThatcheRRR <pavelosipov777@mail.ru>
2020-12-02 15:50:21 +0300

    refactor: change imports and rewrite into arrow funcs

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,47 +1,31 @@
-import React, { useContext, useState, useEffect } from "react"
-import { auth } from "../firebase"
+import React, {useContext, useEffect, useState} from "react"
+import { auth } from "../utils/firebase"
 
-const AuthContext = React.createContext()
+const AuthContext = React.createContext();
 
-export function useAuth() {
-    return useContext(AuthContext)
-}
+export const useAuth = () => useContext(AuthContext);
 
-export function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState()
-    const [loading, setLoading] = useState(true)
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
 
-    function signup(email, password) {
-        return auth.createUserWithEmailAndPassword(email, password)
-    }
+    const signup = (email, password) => auth.createUserWithEmailAndPassword(email, password);
 
-    function login(email, password) {
-        return auth.signInWithEmailAndPassword(email, password)
-    }
+    const login = (email, password) => auth.signInWithEmailAndPassword(email, password);
 
-    function logout() {
-        return auth.signOut()
-    }
+    const logout = () => auth.signOut();
 
-    function resetPassword(email) {
-        return auth.sendPasswordResetEmail(email)
-    }
+    const resetPassword = email => auth.sendPasswordResetEmail(email);
 
-    function updateEmail(email) {
-        return currentUser.updateEmail(email)
-    }
+    const updateEmail = email => currentUser.updateEmail(email);
 
-    function updatePassword(password) {
-        return currentUser.updatePassword(password)
-    }
+    const updatePassword = password => currentUser.updatePassword(password);
 
     useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user)
-            setLoading(false)
-        })
-
-        return unsubscribe
+        return auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+            setLoading(false);
+        });
     }, [])
 
     const value = {
@@ -59,4 +43,4 @@ export function AuthProvider({ children }) {
             {!loading && children}
         </AuthContext.Provider>
     )
-}
+};
